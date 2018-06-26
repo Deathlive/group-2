c0597440daf935b033a2075a37586f5564ed580d
Deathlive <armen1298@gmail.com>
2018-06-26 09:08:13 +0400

    Fixed LoginForm Project

--- HTMLandCSS/LoginForm/javascript/script.js
+++ HTMLandCSS/LoginForm/javascript/script.js
@@ -1,58 +1,134 @@
 $(function() {
-    // $("#myModal").modal("hide");
-    $("#myModal").modal("show");
 
-    $("#phone").forceNumbericOnly();
-    $("#postal").forceNumbericOnly();
+    /* Modal show and hide if */
+    if (sessionStorage.getItem("name") == null) {
+        $("#myModal").modal("show");
+    } else {
+        Load();
+    }
 
+    /* Result load*/
+    if (sessionStorage.getItem("myKey1") != null) {
+        var contentsOfOldDiv = JSON.parse(sessionStorage["myKey1"]);
+        $("#resultBox").html(contentsOfOldDiv);
+    }
+
+    /* Login Form submit function */
     $("#myform1").submit(function() {
+        if ($("#click_me").is(":checked")) {
+            $("#title").html("Hi Mr.");
+        }
+
+        if ($("#or_me").is(":checked")) {
+            $("#title").html("Hi Mrs.");
+        }
+
         push();
+        Save();
         $("#myModal").modal("hide");
         return false;
     });
 
+    /* Contact Form submit function */
     $("#myform2").submit(function() {
-        pushTable();
-        // $("html, body").animate({ scrollTop: $(document).height() }, 800);
+        addResult();
+        resultSave();
+        clearInput();
 
-        $('#sendmessage').hide();
-        $('#table-box').hide();
-        $('#output-box').hide();
-
+        $("#sendmessage").hide();
         $("#sendmessage").fadeIn(800);
-        $("#table-box").fadeIn(3000);
-        $("#output-box").slideDown(1000);
+        $("#sendmessage").fadeOut(800);
 
         return false;
     })
 
 });
 
+/* Push name and surname to header */
 function push() {
     $("#hname").html($("#name").val());
     $("#hlname").html($("#lname").val());
 }
 
-function pushTable() {
-    $("#name1").html($("#prenam").val());
-    $("#name2").html($("#nam").val());
-    $("#phonenum").html($("#phone").val());
-    $("#citi").html($("#ville").val());
-    $("#addr").html($("#adresse").val());
-    $("#postcod").html($("#postal").val());
+/* Clear input value in Contact Form */
+function clearInput() {
+    $("#prenam").val("");
+    $("#nam").val("");
+    $("#phone").val("");
+    $("#ville").val("");
+    $("#adresse").val("");
+    $("#postal").val("");
 }
 
-$.fn.forceNumbericOnly = function() {
-    return this.each(function() {
-        $(this).keydown(function(e) {
-            var key = e.charCode || e.keyCode || 0;
-            return (
-                key == 8 ||
-                key == 9 ||
-                key == 46 ||
-                (key >= 37 && key <= 40) ||
-                (key >= 48 && key <= 57) ||
-                (key >= 96 && key <= 105));
-        });
-    });
-};
+/* Add input value of Contact Form to #resultBox div */
+function addResult() {
+    $("<div/>", { id: "rowBox", class: "row" }).append(
+        $("<div/>", { id: "table-box", class: "col-md-12" }).append(
+            $("<table/>", { class: "table table-hover" }).append(
+                $("<tr/>").append(
+                    $("<td/>").text("Name:"),
+                    $("<td/>", { id: "name1" }).html($("#prenam").val())
+                ),
+                $("<tr/>").append(
+                    $("<td/>").text("Surname:"),
+                    $("<td/>", { id: "name2" }).html($("#nam").val())
+                ),
+                $("<tr/>").append(
+                    $("<td/>").text("Phone:"),
+                    $("<td/>", { id: "phonenum" }).html($("#phone").val())
+                ),
+                $("<tr/>").append(
+                    $("<td/>").text("City:"),
+                    $("<td/>", { id: "citi" }).html($("#ville").val())
+                ),
+                $("<tr/>").append(
+                    $("<td/>").text("Address:"),
+                    $("<td/>", { id: "addr" }).html($("#adresse").val())
+                ),
+                $("<tr/>").append(
+                    $("<td/>").text("Postcode:"),
+                    $("<td/>", { id: "postcod" }).html($("#postal").val())
+                )
+            )
+        )
+    ).appendTo("#resultBox");
+}
+
+/* Save name and surname at header */
+function Save() {
+    gender = "Male";
+
+    if ($("#click_me").is(":checked")) {
+        gender = "Male";
+    }
+    if ($("#or_me").is(":checked")) {
+        gender = "Female";
+    }
+
+    sessionStorage.setItem("name", $("#name").val());
+    sessionStorage.setItem("lname", $("#lname").val());
+    sessionStorage.setItem("gender", gender);
+    Load();
+}
+
+/* Load name and surname at header */
+function Load() {
+    var name1 = sessionStorage.getItem("name");
+    var name2 = sessionStorage.getItem("lname")
+
+    if (sessionStorage.getItem("gender") == "Male") {
+        $("#title").html("Hi Mr.");
+    }
+    if (sessionStorage.getItem("gender") == "Female") {
+        $("#title").html("Hi Mrs.");
+    }
+
+    $("#hname").text(name1);
+    $("#hlname").text(name2);
+}
+
+/* Save result rowBox at resultBox div */
+function resultSave() {
+    var json1 = JSON.stringify($("#resultBox").html());
+    sessionStorage.setItem("myKey1", json1);
+}
